178487fa596103a2fe7765a8026a4d3786a1ea43
ruixify <1492298957@qq.com>
2026-07-08 09:45:42 +0000

    Use existing code to perform simple data transfer between the client and server.

---
+++ examples/client.cpp
@@ -0,0 +1,96 @@
+﻿#include <iostream>
+#include <cerrno>
+#include <cstring>
+#include <fcntl.h>
+
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdexcept>
+
+#include "../include/corotine/task.h"
+#include "../include/runtime/event_loop.h"
+
+
+void set_nonblocking(int fd) {
+    int old_status =  fcntl(fd, F_GETFL, 0);
+    int new_status = old_status | O_NONBLOCK;
+
+    if(fcntl(fd, F_SETFL, new_status) == -1) {
+        throw std::runtime_error(std::strerror(errno));
+    }
+}
+
+
+int create_socket(const char* ip, int port) {
+    int fd = socket(PF_INET, SOCK_STREAM, 0);
+    if(fd == -1) {
+        throw std::runtime_error(std::strerror(errno));
+    }
+
+    struct sockaddr_in clienmtaddr;
+    clienmtaddr.sin_family = AF_INET;
+    inet_pton(AF_INET, ip, &clienmtaddr.sin_addr.s_addr);
+    clienmtaddr.sin_port = htons(port);
+
+    if(connect(fd, (struct sockaddr*)(&clienmtaddr), sizeof(clienmtaddr)) == -1) {
+        close(fd);
+        throw std::runtime_error(std::strerror(errno));
+    }
+
+    set_nonblocking(fd);
+
+    return fd;
+}
+
+Task handle_server(Event_loop& loop, int fd) {
+    char buf[1024];
+
+    while(true) {
+        co_await loop.write(fd);
+
+        char* data = "i am client.";
+        ssize_t written = write(fd, data, strlen(data));
+
+        if(written == -1) {
+            if(errno == EAGAIN || errno == EWOULDBLOCK) {
+                continue;
+            }
+            printf("write error\n");
+            break;
+        }
+
+        sleep(1);
+        co_await loop.read(fd);
+
+        ssize_t n = read(fd, buf, sizeof(buf));
+
+        if(n == 0) {
+            std::cout << "server closed" << std::endl;
+            break;
+        } else if(n == -1) {
+            if(errno == EAGAIN || errno == EWOULDBLOCK) {
+                continue;
+            }
+            printf("read error\n");
+            break;
+        } else {
+            printf("%s\n", buf);
+        }
+    }
+}
+
+int main() {
+    Scheduler sc;
+    Event_loop loop(sc);
+
+    int fd = create_socket("127.0.0.1", 1234);
+
+    sc.spawn(handle_server(loop, fd));
+
+    loop.run();
+
+    close(fd);
+}
---
+++ examples/server.cpp
@@ -0,0 +1,141 @@
+﻿#include <iostream>
+#include <cerrno>
+#include <stdexcept>
+#include <cstring>
+
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <assert.h>
+#include <fcntl.h>
+
+#include "../include/corotine/task.h"
+#include "../include/runtime/event_loop.h"
+
+
+void set_nonblocking(int fd) {
+    int old_status = fcntl(fd, F_GETFL, 0);
+    int new_status = old_status | O_NONBLOCK;
+
+    if(fcntl(fd, F_SETFL, new_status) == -1) {
+        throw std::runtime_error(std::strerror(errno));
+    }
+}
+
+int create_listen_socket(int port) {
+    int listen_fd = socket(PF_INET, SOCK_STREAM, 0);
+    if(listen_fd == -1) {
+        throw std::runtime_error(std::strerror(errno));
+    }
+
+    int op = 1;
+    if(setsockopt(listen_fd, SOL_SOCKET, SO_REUSEADDR, &op, sizeof(op)) == -1) {
+        close(listen_fd);
+        throw std::runtime_error(std::strerror(errno));
+    }
+
+    struct sockaddr_in skaddr;
+    skaddr.sin_family = AF_INET;
+    skaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    skaddr.sin_port = htons(port);
+    if(bind(listen_fd, (struct sockaddr*)(&skaddr), sizeof(skaddr)) == -1) {
+        close(listen_fd);
+        throw std::runtime_error(std::strerror(errno));
+    }
+
+    if(listen(listen_fd, 8) == -1) {
+        close(listen_fd);
+        throw std::runtime_error(std::strerror(errno));
+    }
+
+    set_nonblocking(listen_fd);
+    return listen_fd;
+}
+
+Task handle_client(Event_loop& loop, int client_fd) {
+    char buf[1024];
+
+    while(true) {
+        co_await loop.read(client_fd);
+
+        int n = read(client_fd, buf, sizeof(buf));
+
+        if(n == 0) {
+            std::cout << "client closed" << std::endl;
+            break;
+        }
+
+        if(n == -1) {
+            if(errno == EAGAIN || errno == EWOULDBLOCK) {
+                continue;
+            }
+
+            std::cout << "read error" << std::endl;
+            break;
+        }
+        printf("%s\n", buf);
+
+        co_await loop.write(client_fd);
+        char* data = "i am server.";
+        ssize_t written = write(client_fd, data, strlen(data));
+
+        if(written == -1) {
+            if(errno == EAGAIN || errno == EWOULDBLOCK) {
+                continue;
+            }
+
+            std::cout << "write erro" << std::endl;
+            break;
+        }
+    }
+
+    close(client_fd);
+    co_return;
+}
+
+Task accept_loop(Event_loop& loop, Scheduler& sc, int listen_fd) {
+    while(true) {
+        co_await loop.read(listen_fd);
+
+        while(true) {
+            struct sockaddr_in clientaddr;
+            socklen_t client_len = sizeof(clientaddr);
+            int client_fd = accept(listen_fd, (struct sockaddr*)(&clientaddr), &client_len);
+
+            if(client_fd == -1) {
+                if(errno == EAGAIN || errno == EWOULDBLOCK) {
+                    break;
+                }
+
+                std::cout << "accept erro" << std::endl;
+                break;
+            }
+
+            int port = ntohs(clientaddr.sin_port);
+            set_nonblocking(client_fd);
+
+            std::cout << "client port: " << port << std::endl;
+
+            sc.spawn(handle_client(loop, client_fd));
+        }
+    }
+
+    co_return;
+}
+
+int main() {
+    Scheduler sc;
+    Event_loop loop(sc);
+
+    int listen_fd = create_listen_socket(1234);
+
+    sc.spawn(accept_loop(loop, sc, listen_fd));
+
+    std::cout << "echo server listening on 127.0.0.1:1234" << std::endl;
+
+    loop.run();
+
+    close(listen_fd);
+}
